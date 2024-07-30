1657fd6e97cce02594279973e8aee12bd47d4d4b
yogik23 <61941105+yogik23@users.noreply.github.com>
2024-07-30 20:41:21 +0700

    Update gaian.js

--- gaian.js
+++ gaian.js
@@ -37,8 +37,8 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
                 );
 
                 console.log("Response: [" + response.data.choices[0].message.content + "]\n");
-                console.log("WAIT 2 MENIT \n\n");
-                await delay(120000);
+                console.log("WAIT 1 MENIT \n\n");
+                await delay(600000);
             } catch (postError) {
                 console.error("Error during axios post: ", postError);
             }
